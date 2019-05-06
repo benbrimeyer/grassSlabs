013998e47e374319232c3f9d17ed49dbdeec2f1b
GollyGreg <benjaminbrimeyer@gmail.com>
2019-05-06 00:36:20 -0700

    Set up Mod info

---
+++ src/main/java/com/benj/grassSlabs/Main.java
@@ -0,0 +1,9 @@
+package com.benj.grassSlabs;
+
+import net.minecraftforge.fml.common.Mod;
+import com.benj.grassSlabs.Reference;
+
+@Mod(Reference.MOD_ID)
+public class Main {
+
+}
---
+++ src/main/java/com/benj/grassSlabs/Reference.java
@@ -0,0 +1,5 @@
+package com.benj.grassSlabs;
+
+public class Reference {
+    public static final String MOD_ID = "benj-grass-slabs";
+}
